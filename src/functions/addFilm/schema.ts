ecae35d40b32e53b43ed530552c34c600ba79269
luissevilla <luissevilla111@gmail.com>
2023-01-07 15:14:27 -0600

    defining schema to addFilm

--- src/functions/addFilm/schema.ts
+++ src/functions/addFilm/schema.ts
@@ -1,7 +1,11 @@
 export default {
   type: "object",
   properties: {
-    name: { type: 'string' }
+    name: { type: "string" },
+    description: { type: "string" },
+    imageUrl: { type: "string" },
+    gender: { type: "string" },
+    durationMinutes: { type: "string" },
   },
-  required: ['name']
+  required: ["name", "description", "imageUrl", "gender", "durationMinutes"],
 } as const;
